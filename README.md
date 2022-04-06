15bb45b3da79be3980903abef17a69820137a2e2
dajs64 <dajs64@gmail.com>
2022-04-06 13:33:05 -0400

    set up readme

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 ![the office](https://user-images.githubusercontent.com/100155199/161991126-c41053f8-43c9-433c-be52-f372c2cd8bd5.jpeg)
 
+You play to win.
+
+Player 1 always goes first. 
+Your goal is to get to 5 correct answers before your opponent correctly answers 5 questions.
+Questions are challenging with a few freebies.
+This game is to challenge everything you thought you knew about the office. 
+Please play with caution and refrain from googling an answer.
+Click the right answer to recieve 1 point!
---
+++ app.js
@@ -0,0 +1,40 @@
+// clickable buttons for answers //
+// Start page loads with an option for yes or no //
+// Two players // 
+// Players switch with each question //
+// 1 point assigned for the correct answer // 
+// First to 5 wins // 
+// 3 buttons w/ potential answers // 
+// Questions displayed in the middle of the screen //
+// Correct button pressed point assigned to designated player // 
+// Next page with new question prompt and potential answers appears //
+// Wrong button pressed next page with new question prompt and potential answers appears // 
+// Winner screen appears when a player reaches 5 points //
+// 20 question cap //
+
+                              // ** Start Button ** //
+// create start button
+// add clickable feature for start
+// clickable but non functioning yes/no buttons
+// upon start click link trivia page 
+
+                              // ** Trivia Page ** //
+// create trivia page 
+// create a question area within the middle of the page
+// cycle through questions and answers page by page 
+// DO NOT RANDOMIZE
+// create buttons with potential answers 
+// write if else statements for each page focusing on each specific page (20 total questions) (20 total pages)
+// LINK QUESTIONS TO ANSWERS
+// create an array of 3 potential answers per page (60 total answers) (group 3 answers to each page)
+
+                              // ** Point System ** //
+// add container window with player 1 & player 2 & scores displayed 
+// assign points linked to if else statements (ie. if "true" 1 point/if "false 0 points")
+// create a function to take you to the next question if a button is pressed (only buttons available = answers = 3)
+
+
+                              // ** Winner Window ** // 
+// create a sigular button to pop up upon 5 correct answers 
+// pops up in the middle of the screen 
+// create a "play again" button that pops up 2 seconds (2000 ms) after winner window appears lnking the trivia page
