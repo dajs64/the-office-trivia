ead6cdc9e3c48bddca72579411b39138000fba76
dajs64 <dajs64@gmail.com>
2022-04-10 14:31:39 -0400

    afternoon added filler questions still trying to get reset and score to work

--- app.js
+++ app.js
@@ -1,98 +1,3 @@
-// clickable buttons for answers //
-// Start page loads with an option for yes or no //
-// Two players //
-// Players switch with each question //
-// 1 point assigned for the correct answer //
-// First to 5 wins //
-// 3 buttons w/ potential answers //
-// Questions displayed in the middle of the screen //
-// Correct button pressed point assigned to designated player //
-// Next page with new question prompt and potential answers appears //
-// Wrong button pressed next page with new question prompt and potential answers appears //
-// Winner screen appears when a player reaches 5 points //
-// 20 question cap //
-
-// ** Start Button ** //
-// create start button
-// add clickable feature for start
-// clickable but non functioning yes/no buttons
-// upon start click link trivia page
-
-// ** Trivia Page ** //
-// create trivia page
-// create a question area within the middle of the page
-// cycle through questions and answers page by page
-// DO NOT RANDOMIZE
-// create buttons with potential answers
-// write if else statements for each page focusing on each specific page (20 total questions) (20 total pages)
-// LINK QUESTIONS TO ANSWERS
-// create an array of 3 potential answers per page (60 total answers) (group 3 answers to each page)
-
-// ** Point System ** //
-// add container window with player 1 & player 2 & scores displayed
-// assign points linked to if else statements (ie. if "true" 1 point/if "false 0 points")
-// create a function to take you to the next question if a button is pressed (only buttons available = answers = 3)
-
-// ** Winner Window ** //
-// create a sigular button to pop up upon 5 correct answers
-// pops up in the middle of the screen
-// create a "play again" button that pops up 2 seconds (2000 ms) after winner window appears lnking the trivia page
-
-// Phase 1
-// click start button
-// 1 question will present itself
-// 3 answers will appear below
-// choose right answer to get a point
-// earn 5 points to win
-
-// Phase 2
-// start button
-// console.log ("button clicked")
-// build simple window with one button to click labeled "start"
-// button linked to main game page
-// main game page loads
-// game begins instantly
-// html window with start button
-// simple styling focus on the JS
-// once start is clicked game begins
-// start button linked to game page
-
-// question window middle of screen
-// H2 element linked to JS to change questions upon correct answer
-// new question/page loads no matter the answer
-// correct answer awards point to player
-
-// answers dispayed under question window defined as div 1,2,3 elements
-// 3 potential answers to choose from
-// correct answer chosen awards point to player (link points to player)
-// incorrect answer chosen new question and set of answers populates
-
-// player with 5 correct answers wins
-// 20 questions total
-// build array of questions
-// wrong answers not displayed
-// right answers displayed on page or in console
-
-// ******** //  // ******** //  // ******** //  // ******** //  // ******** //
-//start with an array of objects
-// each index of the array first key value pair (question)
-// next 3 key value pairs (answers)
-// event listener on answers
-// incriemtn to the next index in the array
-
-// const questions = [question 1 what hand does michael shoot with,question 2 how much does Angela weigh]
-// const Question 1 = {
-//      Question
-//      option1: "both",
-//      option2: "right",
-//      option3: "left",
-// };
-
-// const Question 2 = {
-//      option1: "92lbs",
-//      option2: "102lbs",
-//      option3: "82lbs",
-// };
 // global variable
 
 const data = [
@@ -111,6 +16,92 @@ const data = [
     options: ["92", "82", "102"],
     answer: "92",
   },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+  {
+    question: "How much does Kevin weigh?",
+    options: ["502", "202", "302"],
+    answer: "302",
+  },
+
 ];
 
 let index = 0;
@@ -154,18 +145,12 @@ function incrementIndex() {
   if (index < data.length -1) {
     index = index + 1;
    } 
-   //else if (index === data.length - 1) {
-    //window.alert("You won?");
-  //}
+   
+  else if (index === data.length - 1) {
+  window.alert("You won?");
+  }
 }
 
-// function updateHtmlElements() {
-//   const returnedQuestion = returnQuestion();
-//     document.getElementById("question").innerText = returnedQuestion;
-//     document.getElementById("option1-btn").innerText = option1;
-//     document.getElementById("option2-btn").innerText = option2;
-//     document.getElementById("option3-btn").innerText = option3;
-// }
 
 function handleClickAnswer(value,answer) {
     console.log(value, answer)
@@ -222,6 +207,15 @@ document
   .addEventListener("click", () => handleClickAnswer(option3, data[index].answer));
 
   
+ // add reset button 
+//  
+
+//   .getElementById('reset').onclick = function changeContent() {
+//   document.getElementById('reset').textContent = "feed me";
+//   document.getElementById('reset').style = "color: blue";
+// }
+//  .addEventListener("click") => onclick(reset)
+
 // // self.setupUserInterfaceWithData = function() {
 // //   // Add questions to buttons
 // //   var ques = questions[gameQuestions[gameIndex]];
@@ -239,5 +233,3 @@ document
 
 // // constButton1 = document.getElementById(correctAnswers)
 
-// // NEXT STEP IF ELSE STATEMENTS SO THAT THE PAGE WONT GO PAST QUESTION # 3 AND WILL STOP AT QUESTION # 1 //
-// // IF ELSE STATEMTNS WILL DO THIS LOOK THEM UP FRIDAY (TOMORROW) //
