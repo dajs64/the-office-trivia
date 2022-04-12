2244d38b6db6c38d0c779516b12af4b5160cb451
dajs64 <dajs64@gmail.com>
2022-04-12 14:43:15 -0400

    afternoon update

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // global variable
-
+var score = 0;
+const playerScore = document.getElementById("score");
 const data = [
   {
     question: "How much does Angela weight?",
-    options: ["102", "82", "92"], 
-    answer: "82" 
+    options: ["102", "82", "92"],
+    answer: "82",
   },
   {
     question: "How much does Michael weight?",
@@ -101,26 +102,23 @@ const data = [
     options: ["502", "202", "302"],
     answer: "302",
   },
-
 ];
 
 let index = 0;
-let option1  = null
-let option2 = null
-let option3 = null
+let option1 = null;
+let option2 = null;
+let option3 = null;
 
 function updateOptions() {
-  option1 = data[index].options[0]
-  option2 = data[index].options[1]
-  option3 = data[index].options[2]
+  option1 = data[index].options[0];
+  option2 = data[index].options[1];
+  option3 = data[index].options[2];
 }
 
-// calculate score 
-document.write('<p>Congradulations! You scored ' + score + ' points!</p>);
+// calculate score
+// document.write('<p>Congradulations! You scored ' + score + ' points!</p>);
 
 updateOptions();
-
-
 
 function populateScreen(index) {
   const currentSelection = data[index];
@@ -135,7 +133,6 @@ function populateScreen(index) {
 
 populateScreen(0);
 
-
 function returnQuestion() {
   const question = data[index].question;
   option1 = data[index].options[0];
@@ -145,53 +142,48 @@ function returnQuestion() {
 }
 
 function incrementIndex() {
-  if (index < data.length -1) {
+  if (index < data.length - 1) {
     index = index + 1;
-   } 
-   
-  else if (index === data.length - 1) {
-  window.alert("You won?");
+  } else if (index === data.length - 1) {
+    window.alert("You won?");
   }
 }
 
-
-
-function handleClickAnswer(value,answer) {
-    console.log(value, answer)
-    if (value === answer) {
-      console.log("true")
-    }
-    else {
-      console.log("false")
-    }
-    incrementIndex()
-    populateScreen(index)
-    updateOptions()
-}
-
-// adding score 
-var score = 0;
-if (data[2] + 1) {
-  document.getElementById("score").innerHTML = score;
-}
-
-
-
-// popup window function 
-  function basicPopup(url) {
-    popupWindow =
-    window.open(url,'popUpWindow','height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directectories=no status=yes');
+function handleClickAnswer(value, answer) {
+  console.log(value, answer);
+  if (value === answer) {
+    score++;
+    playerScore.innerHTML = score;
+    console.log(score);
+  } else {
+    console.log("false");
   }
+  incrementIndex();
 
+  // function checkProgress(value, index) {
+  //   if (value === 15) {
+  //     document.getElementsByTagName("div");
+  //   }
+  // }
+  // checkProgress();
+  populateScreen(index);
+  updateOptions();
+}
 
-
-// add an valuate answer function 
-
-// function incrementIndex () {
-//   index = index + 1;
-//   const returnedOption = returnOption();
-//   document.getElementById("answer-btn1").textContent = returnedOption
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
 // }
+
+// Get the modal
+// add an valuate answer function
+
+function incrementIndex () {
+  index = index + 1;
+  // const returnedOption = returnOption();
+  // document.getElementById("answer-btn1").textContent = returnedOption
+}
 
 // DONT USE TEXT CONTENT***
 // we want to have one question at a time, so the first question can either display on page load or after hitting start
@@ -208,29 +200,34 @@ document
 
 document
   .getElementById("option1-btn")
-  .addEventListener("click", () => handleClickAnswer(option1, data[index].answer));
+  .addEventListener("click", () =>
+    handleClickAnswer(option1, data[index].answer)
+  );
 
 document
   .getElementById("option2-btn")
-  .addEventListener("click", () => handleClickAnswer(option2, data[index].answer));
+  .addEventListener("click", () =>
+    handleClickAnswer(option2, data[index].answer)
+  );
 
 document
   .getElementById("option3-btn")
-  .addEventListener("click", () => handleClickAnswer(option3, data[index].answer));
+  .addEventListener("click", () =>
+    handleClickAnswer(option3, data[index].answer)
+  );
 
-document
-  .getElementById("reset")
-  .addEventListener("click", () => populateScreen(0))
+document.getElementById("reset").addEventListener("click", () => {
+  score = 0;
+  playerScore.innerHTML = score;
+  populateScreen(0);
+});
 
 
+// Get the modal 
+// var modal = document.getElementById("myModal")
+// // Get the button that opens the modal 
+// var playerScore. = score(15);
 
-
-
-
-
-  
- // add reset button 
-//  
 
 //   .getElementById('reset').onclick = function changeContent() {
 //   document.getElementById('reset').textContent = "feed me";
@@ -254,4 +251,3 @@ document
 // // document.on = ("load", function () { })
 
 // // constButton1 = document.getElementById(correctAnswers)
-
